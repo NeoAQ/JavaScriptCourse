e25928580e7f32edefd5dc1e55e3c65d99534549
Neo04 <neo_aq@yahoo.com>
2021-11-04 17:48:00 -0600

    Promesas

--- ECMA6/promesas.js
+++ ECMA6/promesas.js
@@ -1,1 +1,16 @@
-console.log("Hola");
+const helloPromise= () => {
+    return new Promise((resolve, reject)=>
+    {
+        if(false){
+            resolve("Hey");
+        } else{
+            reject("Ups!");
+        }
+
+        
+    });
+}
+
+helloPromise()
+.then(response=> console.log(response))
+.catch(error=> console.log(error));
